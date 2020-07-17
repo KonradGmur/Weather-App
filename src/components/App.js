dcf63102d40e684a90aec02ef2d52ee66663a1f1
Konrad Gmur <konrad.gmur@gmail.com>
2020-07-17 15:08:12 +0200

    Writing a method to retrieve data from API

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,14 @@ import './App.css';
 class App extends Component {
   state = {
     value: '',
+    date: '',
+    city: '',
+    sunrise: '',
+    sunset: '',
+    temp: '',
+    pressure: '',
+    wind: '',
+    err: '',
   }
 
   handleInputChange = (e) => {
@@ -14,10 +22,26 @@ class App extends Component {
     })
   }
 
+  handleCitySubmit = (e) => {
+    e.preventDefault()
+    const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value},&APPID=5f8843adc098ec73a289be697a274010`;
+    fetch(API)
+      .then(response => {
+        if (response.ok) {
+          return response
+        }
+        throw Error ("Nie udało się pobrać danych z API")
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err))
+
+  }
+
   render() {
     return (
       <div className="App">
-        <Form value={this.state.value} change={this.handleInputChange} />
+        <Form value={this.state.value} change={this.handleInputChange} submit={this.handleCitySubmit} />
         <Result />
       </div>
     );
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Form = (props) => {
   return (
-    <form>
-      <input type="text" value={props.value} placeholder="Wpisz miasto" onChange={props.change}/>
+    <form onSubmit={props.submit}>
+      <input type="text" value={props.value} placeholder="Wpisz miasto" onChange={props.change} />
       <button>Wyszukaj miasta</button>
     </form>
   )
