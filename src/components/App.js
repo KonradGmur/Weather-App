42ab64d936228480b9a38b457e34fd13e658b835
Konrad Gmur <konrad.gmur@gmail.com>
2020-07-17 18:14:30 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -24,36 +24,6 @@ class App extends Component {
     })
   }
 
-  // handleCitySubmit = (e) => {
-  //   e.preventDefault()
-  //   const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value},&APPID=${APIKey}`;
-  //   fetch(API)
-  //     .then(response => {
-  //       if (response.ok) {
-  //         return response
-  //       }
-  //       throw Error("Nie udało się pobrać danych z API")
-  //     })
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       const time = new Date().toLocaleString()
-  //       this.setState(prevState => ({
-  //         err: false,
-  //         date: time,
-  //         sunrise: data.sys.sunrise,
-  //         sunset: data.sys.sunset,
-  //         temp: data.main.temp,
-  //         pressure: data.main.pressure,
-  //         wind: data.wind.speed,
-  //         city: prevState.value,
-  //       }))
-  //     })
-  //     .catch(err => console.log(err), this.setState(prevState => ({
-  //       err: true,
-  //       city: prevState.value
-  //     })))
-  // }
-
   componentDidUpdate(prevProps, prevState) {
     if (this.state.value.length === 0) return
     if (prevState.value !== this.state.value) {
