1aebc452755b9b4c881d725bd5c83b2687d440af
Konrad Gmur <konrad.gmur@gmail.com>
2020-07-17 13:00:57 +0200

    The initial draft was added

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Form from './Form';
+import Result from './Result';
+import './App.css';
+
+class App extends Component {
+  state = {}
+
+  render() {
+    return ( 
+      
+     );
+  }
+}
+
+export default App;
